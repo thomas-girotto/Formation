306c43bb2f8b827b47770b3161fa3b9c6c1d07a0
thomas-girotto <thomas.girotto@gmail.com>
2017-06-05 20:35:56 +0200

    tests with custom FakeSensor

--- AstekTU/Exercice1.Tests/AlarmTest.cs
+++ AstekTU/Exercice1.Tests/AlarmTest.cs
@@ -1,18 +1,60 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NFluent;
+using NUnit.Framework;
 
 namespace Exercice1.Tests
 {
     public class AlarmTest
     {
+        private Alarm _alarm;
+        private FakeSensor _fakeSensor = new FakeSensor();
+
+        [SetUp]
+        public void Setup()
+        {
+            _alarm = new Alarm(_fakeSensor);
+        }
+
         [Test]
         public void Should_raise_an_alarm_When_pressure_is_lower_than_17()
         {
+            // Given
+            _fakeSensor.SetTheNextPressurePsiValue(16);
+            
+            // When
+            _alarm.Check();
 
+            // Then
+            Check.That(_alarm.AlarmOn).IsTrue();
+        }
+
+        [TestCase(17)]
+        [TestCase(18)]
+        [TestCase(19)]
+        [TestCase(20)]
+        [TestCase(21)]
+        public void Should_not_raise_an_alarm_When_pressure_is_between_17_and_21(double givenPressure)
+        {
+            // Given
+            _fakeSensor.SetTheNextPressurePsiValue(givenPressure);
+
+            // When
+            _alarm.Check();
+
+            // Then
+            Check.That(_alarm.AlarmOn).IsFalse();
+        }
+
+        [Test]
+        public void Should_raise_an_alarm_When_pressure_is_greater_than_21()
+        {
+            // Given
+            _fakeSensor.SetTheNextPressurePsiValue(22);
+
+            // When
+            _alarm.Check();
+
+            // Then
+            Check.That(_alarm.AlarmOn).IsTrue();
         }
     }
 }
