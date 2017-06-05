8ce005ed2729f55195a4b3cf281d4e830dbfa5ed
thomas-girotto <thomas.girotto@gmail.com>
2017-06-05 19:46:07 +0200

    add Exercice1

---
+++ AstekTU/Exercice1/Alarm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1
+{
+    public class Alarm
+    {
+        private const double LowPressureThreshold = 17;
+        private const double HighPressureThreshold = 21;
+
+        Sensor _sensor = new Sensor();
+
+        bool _alarmOn = false;
+        private long _alarmCount = 0;
+
+
+        public void Check()
+        {
+            double psiPressureValue = _sensor.PopNextPressurePsiValue();
+
+            if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
+            {
+                _alarmOn = true;
+                _alarmCount += 1;
+            }
+        }
+
+        public bool AlarmOn
+        {
+            get { return _alarmOn; }
+        }
+
+    }
+}
---
+++ AstekTU/Exercice1/Sensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1
+{
+    class Sensor
+    {
+        const double Offset = 16;
+
+        public double PopNextPressurePsiValue()
+        {
+            double pressureTelemetryValue;
+            SamplePressure(out pressureTelemetryValue);
+
+            return Offset + pressureTelemetryValue;
+        }
+
+        private static void SamplePressure(out double pressureTelemetryValue)
+        {
+            // placeholder implementation that simulate a real sensor in a real tire
+            Random basicRandomNumbersGenerator = new Random();
+            pressureTelemetryValue = 6 * basicRandomNumbersGenerator.NextDouble() * basicRandomNumbersGenerator.NextDouble();
+        }
+    }
+}
