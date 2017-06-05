f12448556b5a3a1a0082683ddb9a17d423c162a5
thomas-girotto <thomas.girotto@gmail.com>
2017-06-05 20:35:56 +0200

    tests with custom FakeSensor

---
+++ AstekTU/Exercice1.Tests/FakeSensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1.Tests
+{
+    public class FakeSensor : ISensor
+    {
+        private double _value = 18;
+
+        public void SetTheNextPressurePsiValue(double value)
+        {
+            _value = value;
+        }
+        public double PopNextPressurePsiValue()
+        {
+            return _value;
+        }
+    }
+}
