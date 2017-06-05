12fae400c49c77a9611bffa18074e33599e6084f
thomas-girotto <thomas.girotto@gmail.com>
2017-06-05 20:05:29 +0200

    add empty test project for Exercice1

---
+++ AstekTU/Exercice1.Tests/AlarmTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1.Tests
+{
+    public class AlarmTest
+    {
+        [Test]
+        public void Should_raise_an_alarm_When_pressure_is_lower_than_17()
+        {
+
+        }
+    }
+}
