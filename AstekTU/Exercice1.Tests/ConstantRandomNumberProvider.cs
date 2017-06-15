87d991933108547f4e30a48ecb9c0fb10a18133a
thomas-girotto <thomas.girotto@gmail.com>
2017-06-15 13:04:09 +0200

    ambiant context implementation for Random

---
+++ AstekTU/Exercice1.Tests/ConstantRandomNumberProvider.cs
@@ -0,0 +1,18 @@
+﻿using Exercice1.Utils;
+
+namespace Exercice1.Tests
+{
+    public class ConstantRandomNumberProvider : RandomNumberProvider
+    {
+        private double _theNextDoubleValue;
+
+        public ConstantRandomNumberProvider(double theNextDoubleValue)
+        {
+            _theNextDoubleValue = theNextDoubleValue;
+        }
+        public override double NextDouble()
+        {
+            return _theNextDoubleValue;
+        }
+    }
+}
