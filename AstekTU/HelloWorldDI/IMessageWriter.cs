7bf8e37c1881011fd578171e03f7fb19cf3d0051
thomas-girotto <thomas.girotto@gmail.com>
2017-06-17 22:07:47 +0200

    HelloWorld exercice

---
+++ AstekTU/HelloWorldDI/IMessageWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDI
+{
+    public interface IMessageWriter
+    {
+        void Write(string text);
+    }
+}
