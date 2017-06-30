342fd12ce3e2cda15adfebdd69390bd8d30a3295
thomas-girotto <thomas.girotto@gmail.com>
2017-06-30 21:25:47 +0200

    init WebApi project

--- AstekTU/HelloWorldDI.Tests/SalutationTest.cs
+++ AstekTU/HelloWorldDI.Tests/SalutationTest.cs
@@ -1,4 +1,5 @@
 ﻿using NFluent;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace HelloWorldDI
@@ -6,7 +7,7 @@ namespace HelloWorldDI
     public class SalutationTest
     {
         [Test]
-        public void Should_Write_HelloWorld()
+        public void Should_Write_HelloWorld_with_custom_spy()
         {
             // Given
             var spy = new MessageWriterSpy();
@@ -17,6 +18,24 @@ namespace HelloWorldDI
 
             // Then
             Check.That(spy.TextWritten).IsEqualTo("Hello World");
+        }
+        
+        [Test]
+        public void Should_Write_HelloWorld_With_mock_library()
+        {
+            // Given
+            var fakeMessageWriter = Substitute.For<IMessageWriter>();
+            string writtenText = string.Empty;
+            fakeMessageWriter
+                .When(x => x.Write(Arg.Any<string>()))
+                .Do(args => writtenText = args.Arg<string>());
+            var salutation = new Salutation(fakeMessageWriter);
+
+            // When
+            salutation.Salute();
+
+            // Then
+            Check.That(writtenText).IsEqualTo("Hello World");
         }
     }
 }
